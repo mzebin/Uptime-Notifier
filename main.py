eb2bb8d3ae7ae6bc6663ae7e4662fc53d44dc09f
Mohammed Zebin <mohammed.zebin7@gmail.com>
2021-01-27 10:37:52 +0400

    Updated main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 import os
 import time
 
+OSASCRIPT = """
+osascript -e 'display notification "{} hours since boot up." with title "Uptime-Notifier" subtitle "Warning" sound name "pop"'
+"""
 
 # Functions
 
